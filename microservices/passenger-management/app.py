086946b02aa2c2a008b10fc6db0fd4c1e101c744
vietsimon <vtran1555@gmail.com>
2022-08-22 05:38:33 +0000

    change sqlite to postgres

--- microservices/passenger-management/app.py
+++ microservices/passenger-management/app.py
@@ -1,8 +1,8 @@
 # flask imports
 from flask import Flask, request, jsonify, make_response
 
-import uuid # for public id
-from  werkzeug.security import generate_password_hash, check_password_hash
+import uuid  # for public id
+from werkzeug.security import generate_password_hash, check_password_hash
 # imports for PyJWT authentication
 import jwt
 from datetime import datetime, timedelta
@@ -16,27 +16,33 @@ app = Flask(__name__)
 # INSTEAD CREATE A .env FILE AND STORE IN IT
 app.config['SECRET_KEY'] = 'your secret key'
 # database name
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/passenger.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Nintendo123@172.104.167.232:8201/call-center'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 # creates SQLALCHEMY object
 
 # initialize dabasae
 db.init_app(app)
+db.create_all(app=app)
 app.app_context().push()
-  
+
 # Passenger Database Route
 # this route sends back list of passengers
+
+
 @app.route('/get', methods=['GET'])
 def get_all_passengers():
     # querying the database
     # for all the entries in it
-    data = request.json 
+    data = request.json
     username = data.get('username')
 
     passengers = passenger = Passenger.query\
         .filter_by(username=username)\
         .first()
 
+
+    if (passenger is None):
+        return jsonify({'passengers': "not found"}), 400
     # converting the query objects
     # to list of jsons
     output = []
@@ -45,18 +51,21 @@ def get_all_passengers():
         'email': passenger.email,
         'phone': passenger.phone
     })
-  
-    return jsonify({'passengers': output})
-  
+
+    return jsonify({'passengers': output}), 200
+
 # route for logging passenger in
+
+
 @app.route('/edit', methods=['POST'])
 def login():
     # creates dictionary of form data
     # creates a dictionary of the form data
-    data = request.json 
-  
+    data = request.json
+
     # get data from json data
-    username, email, phone = data.get('username'), data.get('email'), data.get('phone')
+    username, email, phone = data.get(
+        'username'), data.get('email'), data.get('phone')
 
     # checking for existing passenger
     passenger = Passenger.query\
@@ -68,21 +77,22 @@ def login():
         passenger.email = email
         passenger.phone = phone
         db.session.commit()
-  
-        return jsonify({'message' : 'Successfully edited.'}), 201 
+
+        return jsonify({'message': 'Successfully edited.'}), 201
     else:
         # returns 401 if passenger already exists
-        return jsonify({'message' : 'Passenger is not exists'}), 401 
-    
-  
+        return jsonify({'message': 'Passenger is not exists'}), 401
+
+
 # create route
 @app.route('/create', methods=['POST'])
 def create():
     # creates a dictionary of the form data
-    data = request.json 
-  
+    data = request.json
+
     # get data from json data
-    username, email, phone = data.get('username'), data.get('email'), data.get('phone')
+    username, email, phone = data.get(
+        'username'), data.get('email'), data.get('phone')
 
     # checking for existing passenger
     passenger = Passenger.query\
@@ -91,19 +101,18 @@ def create():
     if not passenger:
         # database ORM object
         passenger = Passenger(
-            username = username,
-            email = email,
-            phone = phone
+            username=username,
+            email=email,
+            phone=phone
         )
         # insert passenger
         db.session.add(passenger)
         db.session.commit()
-  
-        return jsonify({'message' : 'Successfully created.'}), 201 
+
+        return jsonify({'message': 'Successfully created.'}), 201
     else:
         # returns 401 if passenger already exists
-        return jsonify({'message' : 'Passenger already exists.'}), 401 
-
+        return jsonify({'message': 'Passenger already exists.'}), 401
 
 
 if __name__ == "__main__":
--- microservices/passenger-management/models.py
+++ microservices/passenger-management/models.py
@@ -7,6 +7,6 @@ db = SQLAlchemy()
 # Database ORMs
 class Passenger(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(70), unique = True)
-    email = db.Column(db.String(70))
-    phone = db.Column(db.String(70))
+    username = db.Column(db.Text(70), unique = True)
+    email = db.Column(db.Text(70))
+    phone = db.Column(db.Text(70))
